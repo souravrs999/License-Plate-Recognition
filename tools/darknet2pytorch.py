8fe351fdaefcd36034bd6d156d096828eec905e3
souravrs999 <souravraveendran6@gmail.com>
2020-11-13 09:49:33 +0530

    fixed imports

--- tools/darknet2pytorch.py
+++ tools/darknet2pytorch.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from tool.region_loss import RegionLoss
-from tool.yolo_layer import YoloLayer
-from tool.config import *
-from tool.torch_utils import *
+from tools.region_loss import RegionLoss
+from tools.yolo_layer import YoloLayer
+from tools.config import *
+from tools.torch_utils import *
 
 
 class Mish(torch.nn.Module):
